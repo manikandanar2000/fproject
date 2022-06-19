0f5ed83897a7b3d80862d11acb5f984c288e0b7d
manikandanar2000 <mani4.reghu@gmail.com>
2022-06-19 11:29:07 +0530

    second commit

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,18 +4,18 @@ import './Banners.css';
 function Banner() {
   return (
     <div>
-       <div className='banner'>
+     <div className='banner'>
       <div className='content'>
-          <h1 className='title'>MOVIE NAME</h1>
+          <h1 className='title'>welcome</h1>
           <div className='bannerbutton'>
-              <button className='button'>Play</button>
-              <button className='button'>Mylist</button>
+              {/* <button className='button'>Play</button>
+              <button className='button'>Mylist</button> */}
 
           </div>
           <h1 className='discription'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a</h1>
 
       </div>
-      <div className="fade_bottom"></div>
+      
     </div>
     </div>
   )
--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <div  className="navbar">
         
-      <img  className="logo" src=" " alt="Netflix logo "/>
+      <img  className="logo" src="https://comps.gograph.com/ggb/86189430 " alt="Netflix logo "/>
        <img  className= "avt" src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avtar"/>
      
      
